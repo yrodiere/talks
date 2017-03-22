835af21232730c92f667965bc986003bbec9f7ae
Yoann Rodière <yoann@hibernate.org>
2017-03-22 14:44:29 +0100

    Add reveal.js application template

---
+++ Gruntfile.js
@@ -0,0 +1,97 @@
+module.exports = function(grunt) {
+	var port = grunt.option('port') || 8000;
+	var root = grunt.option('root') || '.';
+
+	if (!Array.isArray(root)) root = [root];
+
+	// Project configuration
+	grunt.initConfig({
+		pkg: grunt.file.readJSON('package.json'),
+
+		sass: {
+			core: {
+				files: {
+					'css/style.css': 'css/style.scss'
+				}
+			},
+			themes: {
+				files: [
+					{
+						expand: true,
+						cwd: 'css/theme/source',
+						src: ['*.scss'],
+						dest: 'css/theme',
+						ext: '.css'
+					}
+				]
+			}
+		},
+
+		autoprefixer: {
+			dist: {
+				src: 'css/style.css'
+			}
+		},
+
+		cssmin: {
+			compress: {
+				files: {
+					'css/style.min.css': [ 'css/style.css' ]
+				}
+			}
+		},
+
+		connect: {
+			server: {
+				options: {
+					port: port,
+					base: root,
+					livereload: true,
+					open: true
+				}
+			}
+		},
+
+		watch: {
+			theme: {
+				files: [ 'css/theme/source/*.scss', 'css/theme/template/*.scss' ],
+				tasks: 'css-themes'
+			},
+			css: {
+				files: [ 'css/style.scss' ],
+				tasks: 'css-core'
+			},
+			html: {
+				files: root.map(path => path + '/*.html')
+			},
+			markdown: {
+				files: root.map(path => path + '/*.md')
+			},
+			options: {
+				livereload: true
+			}
+		}
+
+	});
+
+	// Dependencies
+	grunt.loadNpmTasks( 'grunt-contrib-cssmin' );
+	grunt.loadNpmTasks( 'grunt-contrib-watch' );
+	grunt.loadNpmTasks( 'grunt-sass' );
+	grunt.loadNpmTasks( 'grunt-contrib-connect' );
+	grunt.loadNpmTasks( 'grunt-autoprefixer' );
+
+	// Default task
+	grunt.registerTask( 'default', [ 'css' ] );
+
+	// CSS
+	grunt.registerTask( 'css-themes', [ 'sass:themes' ] );
+	grunt.registerTask( 'css-core', [ 'sass:core', 'autoprefixer', 'cssmin' ] );
+	grunt.registerTask( 'css', [ 'sass', 'autoprefixer', 'cssmin' ] );
+
+
+	// Serve presentation locally
+	grunt.registerTask( 'serve', [ 'connect', 'watch' ] );
+
+};
+
---
+++ README.md
@@ -0,0 +1,21 @@
+# How to launch this presentation
+
+1. Install [Node.js](http://nodejs.org/) and [Grunt](http://gruntjs.com/getting-started#installing-the-cli)
+
+   On Fedora:
+   ```sh
+   $ sudo dnf install npm nodejs-grunt
+   ```
+
+2. Install dependencies of this project (run within the git checkout):
+   ```sh
+   $ npm install
+   ```
+
+3. Serve the presentation (run within the git checkout):
+   ```sh
+   $ npm start
+   ```
+
+4. Open <http://localhost:8000> to view your presentation.
+
